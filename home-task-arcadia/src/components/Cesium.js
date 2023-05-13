1e537c69be8f96a9d390a7180ff1ab36f414d731
ndndnd24 <ndndnd24@gmail.com>
2023-05-13 13:15:42 +0200

    fixed the cesium UI elements to fit on the map

--- home-task-arcadia/src/components/Cesium.js
+++ home-task-arcadia/src/components/Cesium.js
@@ -1,14 +1,54 @@
 "use client";
 
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+// import * as Cesium from "cesium";
+
+// const MapComponent = () => {
+//   Cesium.Ion.defaultAccessToken = process.env.CESIUM_API_TOKEN;
+
+//   useEffect(() => {
+//     // Initialize Cesium viewer
+//     const viewer = new Cesium.Viewer("cesiumContainer", {
+//       terrainProvider: Cesium.createWorldTerrain(),
+//     });
+
+//     // Set initial camera position
+//     viewer.camera.flyTo({
+//       destination: Cesium.Cartesian3.fromDegrees(19.0532484, 47.4986567, 10000),
+//     });
+
+//     // Add a point at Budapest's coordinates
+//     const entity = viewer.entities.add({
+//       position: Cesium.Cartesian3.fromDegrees(19.0532484, 47.4986567),
+//       point: {
+//         pixelSize: 10,
+//         color: Cesium.Color.RED,
+//         outlineColor: Cesium.Color.WHITE,
+//         outlineWidth: 2,
+//       },
+//     });
+
+//     return () => {
+//       // Clean up Cesium viewer
+//       if (viewer && !viewer.isDestroyed()) {
+//         viewer.destroy();
+//       }
+//     };
+//   }, []);
+
+//   return <div style={{ height: "100vh" }} id="cesiumContainer" />;
+// };
+
+// export default MapComponent;
+
+import React, { useEffect } from 'react';
 import * as Cesium from "cesium";
+import 'cesium/Build/Cesium/Widgets/widgets.css';
 
 const MapComponent = () => {
-  Cesium.Ion.defaultAccessToken = process.env.CESIUM_API_TOKEN;
-  
   useEffect(() => {
     // Initialize Cesium viewer
-    const viewer = new Cesium.Viewer("cesiumContainer", {
+    const viewer = new Cesium.Viewer('cesiumContainer', {
       terrainProvider: Cesium.createWorldTerrain(),
     });
 
@@ -36,7 +76,19 @@ const MapComponent = () => {
     };
   }, []);
 
-  return <div style={{ height: "100vh" }} id="cesiumContainer" />;
+  return (
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100vh',
+        overflow: 'hidden',
+      }}
+    >
+      <div id="cesiumContainer" style={{ width: '100%', height: '100%' }} />
+    </div>
+  );
 };
 
 export default MapComponent;
+
