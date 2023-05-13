c9a0ed3c4819f93beb51a0ebd0134a6e90d93041
ndndnd24 <ndndnd24@gmail.com>
2023-05-13 11:56:28 +0200

    removed googlesignin component

--- home-task-arcadia/next.config.js
+++ home-task-arcadia/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    GOOGLE_API_TOKEN: process.env.GOOGLE_API_TOKEN,
+  },
+};
--- home-task-arcadia/src/app/page.js
+++ home-task-arcadia/src/app/page.js
@@ -1,13 +1,40 @@
+"use client";
+
 import Head from "next/head";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { useState } from "react";
 
 export default function Home() {
+  const googleApiToken = process.env.GOOGLE_API_TOKEN;
+  const [isSignedIn, setSignIn] = useState(false);
+
+  function renderMain(isAuthenticated) {
+    if (isAuthenticated) {
+      return <>some body</>;
+    } else {
+      return (
+        <GoogleOAuthProvider clientId={googleApiToken}>
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              setSignIn(true);
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </GoogleOAuthProvider>
+      );
+    }
+  }
+
   return (
     <div>
       <Head>
         <title>Home Task for Arcadia</title>
       </Head>
       <header>some header</header>
-      <main>some body</main>
+      <main>{renderMain(isSignedIn)}</main>
       <footer>some footer</footer>
     </div>
   );
