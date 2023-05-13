c68af3d317f852561f24453021bf9109cc20679a
ndndnd24 <ndndnd24@gmail.com>
2023-05-13 15:21:14 +0200

    main screen css added

--- home-task-arcadia/src/app/page.js
+++ home-task-arcadia/src/app/page.js
@@ -7,14 +7,14 @@ import MapComponent from "@/components/Cesium";
 
 export default function Home() {
   const googleApiToken = process.env.GOOGLE_API_TOKEN;
-  const [isSignedIn, setSignIn] = useState(true);
+  const [isSignedIn, setSignIn] = useState(false);
 
   function renderMain(isAuthenticated) {
     if (isAuthenticated) {
       return <MapComponent />;
     } else {
       return (
-        <div style={{ height: "90vh" }}>
+        <div style={{ height: "90vh" }} id="google-auth-button">
           <GoogleOAuthProvider clientId={googleApiToken}>
             <GoogleLogin
               onSuccess={(credentialResponse) => {
@@ -36,9 +36,16 @@ export default function Home() {
       <Head>
         <title>Home Task for Arcadia</title>
       </Head>
-      <header style={{ height: "5vh" }} id="app-header">some header</header>
+      <header style={{ height: "5vh" }} id="app-header">
+        <div>Home Task for Arcadia by ND</div>
+      </header>
       <main>{renderMain(isSignedIn)}</main>
-      <footer style={{ height: "5vh" }} id="app-footer">some footer</footer>
+      <footer style={{ height: "5vh" }} id="app-footer">
+        <div>
+          <p>Powered by NextJS, CesiumJS, GoogleOAuth.</p>
+          <p>Created by Dávid Németh, 2023.</p>
+        </div>
+      </footer>
     </div>
   );
 }
