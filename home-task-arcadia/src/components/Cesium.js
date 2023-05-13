82e1a6ec960cdaa70c3e8f6462ff3895ab469edf
ndndnd24 <ndndnd24@gmail.com>
2023-05-13 12:37:59 +0200

    cesium basic code that is working

--- home-task-arcadia/src/components/Cesium.js
+++ home-task-arcadia/src/components/Cesium.js
@@ -7,29 +7,41 @@ window.CESIUM_BASE_URL = "/static/Cesium/";
 import * as Cesium from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 
+import { useEffect } from "react";
+import { createWorldTerrain, Viewer } from "cesium";
+
 const MapComponent = () => {
-  // // Your access token can be found at: https://ion.cesium.com/tokens.
-  // // This is the default access token from your ion account
-  Cesium.Ion.defaultAccessToken = process.env.CESIUM_API_TOKEN;
+  useEffect(() => {
+    const viewer = new Viewer("cesiumContainer", {
+      terrainProvider: createWorldTerrain(),
+    });
+  }, []);
+  return <div id="cesiumContainer" />;
 
-  // Initialize the Cesium Viewer in the HTML element with the "cesiumContainer" ID.
-  const viewer = new Cesium.Viewer("cesiumContainer", {
-    terrainProvider: Cesium.createWorldTerrain(),
-  });
-  // Add Cesium OSM Buildings, a global 3D buildings layer.
-  const buildingTileset = viewer.scene.primitives.add(
-    Cesium.createOsmBuildings()
-  );
-  // Fly the camera to San Francisco at the given longitude, latitude, and height.
-  viewer.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-    orientation: {
-      heading: Cesium.Math.toRadians(0.0),
-      pitch: Cesium.Math.toRadians(-15.0),
-    },
-  });
+  //   // // Your access token can be found at: https://ion.cesium.com/tokens.
+  //   // // This is the default access token from your ion account
+  //   Cesium.Ion.defaultAccessToken = process.env.CESIUM_API_TOKEN;
 
-  return <div style={{ height: "100vh" }}>{viewer}</div>;
+  //   // Initialize the Cesium Viewer in the HTML element with the "cesiumContainer" ID.
+  //   useEffect(() => {
+  //     const viewer = new Cesium.Viewer("cesiumContainer", {
+  //       terrainProvider: Cesium.createWorldTerrain(),
+  //     });
+  //   }, []);
+  //   // Add Cesium OSM Buildings, a global 3D buildings layer.
+  //   const buildingTileset = viewer.scene.primitives.add(
+  //     Cesium.createOsmBuildings()
+  //   );
+  //   // Fly the camera to San Francisco at the given longitude, latitude, and height.
+  //   viewer.camera.flyTo({
+  //     destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+  //     orientation: {
+  //       heading: Cesium.Math.toRadians(0.0),
+  //       pitch: Cesium.Math.toRadians(-15.0),
+  //     },
+  //   });
+
+  //   return <div id="cesiumContainer" style={{ height: "100vh" }}></div>;
 };
 
 export default MapComponent;
